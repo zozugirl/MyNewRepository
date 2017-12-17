e29de8a522e700f5137dd6b16306e9e469a5645a
zozugirl <georgina.parnis.b31225@mcast.edu.mt>
2017-12-17 14:07:02 +0100

    make the buttons either go to level 1 or quit(only when it is build)
    Signed-off-by: zozugirl <georgina.parnis.b31225@mcast.edu.mt>

---
+++ compAirHockey/Assets/scripts/menuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class menuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void startGame(){
+		SceneManager.LoadScene (1);
+
+	}
+	public void quitGame(){
+		Application.Quit();
+
+	}
+}
